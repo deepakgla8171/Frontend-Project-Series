3a1a93150804355d00ba62993b30b2f5c19e5e82
Deepak Kumar <dk1184366@gmail.com>
2026-06-28 22:20:29 +0530

    commit

--- Random Color/script.js
+++ Random Color/script.js
@@ -20,15 +20,3 @@ function generateColors() {
     });
 
 }
-
-function randomColor() {
-    const chars = "0123456789abcdef";
-
-    let colorCode = "";
-    for (let index = 0; index < 6; index++) {
-        const randomNum = Math.floor(Math.random() * chars.length);
-        colorCode += chars.substring(randomNum, randomNum + 1);
-
-    }
-    return colorCode;
-}
